aef4d0a9db4508873005e5b2e9e0ed8120c1281b
Neel Redkar <neel.redkar@gmail.com>
2020-07-08 09:54:35 -0700

    escape

--- pages/api/blob.js
+++ pages/api/blob.js
@@ -1,16 +1,54 @@
 import fetch from "node-fetch"
 
-export default async (req, res) => {
-    let urlRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/g;
-    let urls = req.body.text.match(urlRegex)
-    let blob = req.body.text
-    await Promise.all(
-        urls.map(async url => {
-            let response = await fetch(`https://u.nu/api.php?action=shorturl&format=json&url=${url}`)
-            let json = await response.json()
-            blob = blob.replace(new RegExp(url, 'g'), json.shorturl)
-        })
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another option
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
     )
-
-    res.send(blob)
+    if (req.method === 'OPTIONS') {
+        res.status(200).end()
+        return
+    }
+    return await fn(req, res)
 }
+
+const BlobShort = async (req, res) => {
+    if (!req.body.text) {
+        res.send({
+            result: null,
+            error: true
+        })
+        return
+    }
+    let urlRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/g;
+    let urls = [...req.body.text.matchAll(urlRegex)]
+    let blob = req.body.text
+
+    urls ?
+        await Promise.all(
+            urls.map(async url => {
+                console.log(url[0])
+                try {
+                    let response = await fetch(`https://u.nu/api.php?action=shorturl&format=json&url=${encodeURI(url[0])}`)
+                    let json = await response.json()
+                    if (json.shorturl) {
+                        console.log(url[0])
+                        blob = blob.replace(new RegExp(url[0].replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&')
+                            , "gi"), json.shorturl)
+                        console.log(blob)
+                    }
+                } catch {
+                    return
+                }
+            })
+        ) : null
+
+    res.send({ result: blob })
+}
+
+export default allowCors(BlobShort)
--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,48 @@
-export default () => <h1>Hewwo might build a ui later</h1>
+import { Flex, Heading, Button, Text } from "rebass"
+import Head from "next/head"
+
+export default () => (
+    <Flex w="100vw" h="100vh" flexDirection="column">
+        <Head>
+            <title>BlobShort</title>
+        </Head>
+        <Heading mt="50px" mx="auto" fontSize={[4, 5, 6]}>BlobShort</Heading>
+        <Text mx="auto">Shorten all the urls in the text!</Text>
+        <Flex mx="auto" mt="20px">
+            <textarea id="in" />
+        </Flex>
+        <Button sx={{
+            ":hover": {
+                bg: "secondary",
+                cursor: "pointer"
+            }
+        }} mt="50px" mx="auto" onClick={() => {
+            fetch("/api/blob", {
+                body: JSON.stringify({
+                    text: document.getElementById("in").value
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                method: "POST"
+            })
+                .then(r => r.json())
+                .then(json => {
+                    document.getElementById("out").value = json.result
+                })
+        }}>Shorten!</Button>
+        <Flex mx="auto" mt="50px">
+            <textarea id="out" readOnly />
+        </Flex>
+        <Heading mt="60px" mx="auto">API</Heading>
+        <Text p="30px" mx="auto"><strong>POST</strong> <code>/api/blob</code> with a text JSON key for your blob</Text>
+
+        <style jsx>{`
+            textarea {
+                border-radius:10px;
+                height:100px;
+                width:200px;
+            }
+        `}</style>
+    </Flex>
+)
