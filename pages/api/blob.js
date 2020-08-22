97e58476e2d8e80bc4702c1442992ec6a444f4f4
Neel Redkar <neel.redkar@gmail.com>
2020-08-21 19:55:22 -0700

    add stuffs

--- pages/api/blob.js
+++ pages/api/blob.js
@@ -25,22 +25,18 @@ const BlobShort = async (req, res) => {
         })
         return
     }
-    let urlRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/g;
+    let urlRegex = /(https?|chrome):\/\/[^\s$.?#].[^\s]*/g;
     let urls = [...req.body.text.matchAll(urlRegex)]
     let blob = req.body.text
-
     urls ?
         await Promise.all(
             urls.map(async url => {
-                console.log(url[0])
                 try {
-                    let response = await fetch(`https://u.nu/api.php?action=shorturl&format=json&url=${encodeURI(url[0])}`)
+                    let response = await fetch(`https://u.nu/api.php?action=shorturl&format=json&url=${encodeURIComponent(url[0])}`)
                     let json = await response.json()
                     if (json.shorturl) {
-                        console.log(url[0])
                         blob = blob.replace(new RegExp(url[0].replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&')
                             , "gi"), json.shorturl)
-                        console.log(blob)
                     }
                 } catch {
                     return
